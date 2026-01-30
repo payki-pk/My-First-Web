b09081c033bf2a6f43b467e44ae7935e7d74e335
payki-pk <phyokonaing.pk@gmail.com>
2026-01-30 22:41:46 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("JavaScript is working!");
-document.getElementById("title").style.color = "red";
+// ၁။ ခလုတ်ကို ID ကနေတစ်ဆင့် လှမ်းကိုင်တာ
+const phoneElement = document.getElementById('phoneText');
+
+// ၂။ အဲ့ဒီခလုတ်ကို နှိပ်လိုက်ရင် (onclick)
+phoneElement.onclick = function() {
+    
+    // ၃။ ပေါ်လာစေချင်တဲ့ စာသား
+    alert("မင်္ဂလာပါ! ကျွန်တော့်ကို ဆက်သွယ်ပေးလို့ ကျေးဇူးတင်ပါတယ်ခင်ဗျာ။");
+};
